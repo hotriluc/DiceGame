d5cbc1b475cca5482832e688ca344c91e505cd48
hotriluc <hotriluc97@gmail.com>
2020-08-13 20:29:54 +0300

    Init Game
    - Adding function that will initialize the game by resetting gameboard
    - Disabling buttons roll and hold when there's a winner (to enable them you have to start a new game)
    - Also removing possibility of holding 0 value

--- app.js
+++ app.js
@@ -11,20 +11,66 @@ GAME RULES:
 
 //Creating array which holds scores of 2 players;
 //[0] for first player [1] for second;
-
+/*======================VARIABLES======================*/
 var scores, roundScore, activePlayer, newGame;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
 
-//Setting score to 0 on ouh html
-document.querySelector('.gameboard__dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+/*==============FUNCTIONS==============================*/
+//Will reset the gameboard
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    //Setting score to 0 on ouh html
+    document.querySelector('.gameboard__dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    
+    //Setting names
+    document.getElementById("name-0").textContent = 'Player 1';
+    document.getElementById("name-1").textContent = 'Player 2';
+
+    //Removing player state classes
+    document.querySelector(".gameboard__player-panel--0").classList.remove('active');
+    document.querySelector(".gameboard__player-panel--1").classList.remove('active');
+    document.querySelector(".gameboard__player-panel--0").classList.remove('winner');
+    document.querySelector(".gameboard__player-panel--1").classList.remove('winner');
+
+    //First player styling
+    document.querySelector(".gameboard__player-panel--0").classList.add('active');
+
+    //Enabling buttons
+    document.querySelector(".btn-roll").disabled = false;
+    document.querySelector(".btn-hold").disabled = false;
+
+}
+
+// function that change turn of the player
+function changePlayer() {
+    
+    //reseting round score and remove active state 
+    roundScore = 0;
+    document.getElementById('current-'+activePlayer).textContent = roundScore;
+    document.querySelector(".gameboard__player-panel--"+activePlayer).classList.remove('active');
+    
+    //Checking what player is playing right now and changing active player to another
+    activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+    //Showing on UI that another player is active right now
+    document.querySelector(".gameboard__player-panel--"+activePlayer).classList.add('active');
+    console.log('Curent active player'+activePlayer);
+
+    //if he hit 1 dice won't be shown 
+    document.querySelector('.gameboard__dice').style.display = 'none';
+}
+
+/*============================================*/
+init();
 
 
+/*==================EVENT LISTENERS==========================*/
 //On click we going to roll dice 
 document.querySelector('.btn-roll').addEventListener('click',  function(){
     //Getting random nubmer from 1 to 6
@@ -67,20 +113,8 @@ document.querySelector('.btn-roll').addEventListener('click',  function(){
         //IF current (active) player
         // hits 1 then all roundscores that he has received will be reset
         // and on UI we should remove active state and set round score to 0 for this player
-        roundScore = 0;
-        document.getElementById('current-'+activePlayer).textContent = roundScore;
-        document.querySelector("div.gameboard__player-panel--"+activePlayer).classList.remove('active');
-        
-        //Checking what player is playing right now and changing active player to another
-        activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
-        //Showing on UI that another player is active right now
-        document.querySelector("div.gameboard__player-panel--"+activePlayer).classList.add('active');
-        console.log('Curent active player'+activePlayer);
-
-        //if he hit 1 dice won't be shown 
-        diceDOM.style.display = 'none';
-
-       
+        changePlayer();
+  
         //The same with if statement
         // if(activePlayer===1) {
         //     activePlayer = 0;
@@ -93,3 +127,25 @@ document.querySelector('.btn-roll').addEventListener('click',  function(){
 
     }
 );
+
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    if(roundScore===0) {
+        alert("You can not hold 0");
+    } else {
+        scores[activePlayer]+=roundScore;
+        document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+        //Checking if there is a winner
+        if(scores[activePlayer]>=100) {
+            document.getElementById("name-"+activePlayer).textContent = 'Winner !';
+            document.querySelector(".gameboard__player-panel--"+activePlayer).classList.add('winner');
+            document.querySelector(".btn-roll").disabled = true;
+            document.querySelector(".btn-hold").disabled = true;
+        } else {
+            //Changing the player if not
+            changePlayer();
+        }
+    }   
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
